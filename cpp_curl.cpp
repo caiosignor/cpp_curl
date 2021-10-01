450e0cdbb00d70b770c074f7b02648115b37008c
caiosignor <caiosignor@gmail.com>
2021-10-01 18:51:40 -0300

    return cJSON objects

--- cpp_curl.cpp
+++ cpp_curl.cpp
@@ -3,6 +3,8 @@
 #include "cJSON/cJSON.h"
 #include "private.h"
 
+using namespace std;
+
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     ((std::string *)userp)->append((char *)contents, size * nmemb);
@@ -10,7 +12,7 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 }
 
 //cURL make a http post request
-std::string curlPost(std::string url, std::string data)
+std::string curlPost(std::string url, cJSON *data)
 {
     CURL *curl;
     CURLcode res;
@@ -19,7 +21,7 @@ std::string curlPost(std::string url, std::string data)
     if (curl)
     {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, cJSON_Print(data));
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         res = curl_easy_perform(curl);
@@ -28,32 +30,34 @@ std::string curlPost(std::string url, std::string data)
     return readBuffer;
 }
 
-void get()
+cJSON *get(std::string url)
 {
     CURL *curl;
     CURLcode res;
     std::string readBuffer;
-
+    cJSON *ret = NULL;
     curl = curl_easy_init();
     if (curl)
     {
-        curl_easy_setopt(curl, CURLOPT_URL, PRIVATE_URL);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
 
-        std::cout << readBuffer << std::endl;
-        cJSON *json = cJSON_Parse(readBuffer.c_str());
-        cJSON *slot = cJSON_GetObjectItem(json, "slot");
-        printf("%d", slot->valueint);
-        cJSON_Delete(json);
+        ret = cJSON_Parse(readBuffer.c_str());
     }
+    return ret;
 }
 
 int main()
 {
-    get();
-    curlPost(PRIVATE_URL, "{\"slot\":51}");
+    cJSON *saved_files = get(PRIVATE_URL);
+    cout << "Saved files" << cJSON_Print(saved_files) << endl;
+    cJSON *data = cJSON_CreateObject();
+    cJSON_AddStringToObject(data, "name", "test");
+    cJSON_AddNumberToObject(data, "slot", 1);
+    std::string result = curlPost(PRIVATE_URL, data);
+    cout << result << endl;
     return 0;
 }
